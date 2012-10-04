d9689d98951edd58fb6639d85255e868619a2b51
Vladimir Magyar <magyarvladimir@gmail.com>
2012-10-04 09:34:51 +0200

    pridany about box

--- skeleton.py
+++ skeleton.py
@@ -14,18 +14,18 @@ class Particle:
 
 
 class DrawArea:
-    def __init__(self, drawarea):
+    def set_drawarea(self, drawarea):
         self.drawarea = drawarea
-        self.width = self.drawarea.get_allocation().get_width()
-        self.height = self.drawarea.get_allocation().get_height()
+        self.width = self.drawarea.get_allocation().width
+        self.height = self.drawarea.get_allocation().height
 
     def draw(self, drawarea, cr):
         #TODO vykreslit kazdu strunu medzi casticami
         pass
     
     def resize(self, widget, allocation):
-        tra = np.array([[self.width/allocation.get_width(), 0, 0],
-                        [0, self.height/allocation.get_height(), 0],
+        tra = np.array([[self.width/allocation.width, 0, 0],
+                        [0, self.height/allocation.height, 0],
                         [0, 0, 1]])
         #TODO presunut kazdu casticu a jej rychlost
 
@@ -33,16 +33,23 @@ class DrawArea:
 class MainWindow(ObjGetter):
     def __init__(self):
         path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'main.glade')
+        self.darea = DrawArea()
         ObjGetter.__init__(self, path, self.get_signals())
-        self.darea = DrawArea(self.drawarea)
+        self.darea.set_drawarea(self.drawarea)
         self.window.show()
 
     def get_signals(self):
         signals = {"destroy" : Gtk.main_quit,
                    "forward" : self.forward,
                    "draw" : self.darea.draw,
-                   "resize" : self.darea.resize}
+                   "resize" : self.darea.resize,
+                   "about" : self.about}
         return signals
+
+    def about(self, item):
+        resp = self.aboutdialog.run()
+        if resp == Gtk.ResponseType.DELETE_EVENT or resp == Gtk.ResponseType.CANCEL:
+            self.aboutdialog.hide()
 
     def forward(self, button=None):
         #TODO
